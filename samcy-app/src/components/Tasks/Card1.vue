662dd5fee8238eb9de8ddea4c927b720c6f7e783
moshood <kazeemmoshood123@gmail.com>
2025-04-17 03:00:26 -0700

    add task folder

---
+++ samcy-app/src/components/Tasks/Card1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+        <h2>Task Card</h2>
+        <p>This is a task card component.</p>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'Card1',
+}
+</script>
+
+<style>
+
+</style>
